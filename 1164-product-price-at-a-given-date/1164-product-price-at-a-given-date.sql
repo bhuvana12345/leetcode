f5ecf03d7c7027e0e2a9ef8b302d9e8737645b21
Bhuvana Korrapati <bhuvanakorrapati@gmail.com>
2024-12-05 23:55:50 -0500

    Time: 576 ms (42.20%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,22 +1,20 @@
-WITH LatestPrice AS (
-    SELECT
-        product_id,
-        new_price,
-        change_date
+SELECT 
+    p.product_id, 
+    COALESCE(lp.new_price, 10) AS price
+FROM 
+    (SELECT DISTINCT product_id FROM Products) p
+LEFT JOIN (
+    SELECT 
+        product_id, 
+        new_price
     FROM Products
     WHERE change_date <= '2019-08-16'
-),
-RankedPrices AS (
-    SELECT
-        product_id,
-        new_price,
-        ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS rnk
-    FROM LatestPrice
-)
-SELECT
-    p.product_id,
-    COALESCE(rp.new_price, 10) AS price
-FROM (SELECT DISTINCT product_id FROM Products) p
-LEFT JOIN RankedPrices rp
-  ON p.product_id = rp.product_id AND rp.rnk = 1;
+    AND (product_id, change_date) IN (
+        SELECT product_id, MAX(change_date)
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    )
+) lp
+ON p.product_id = lp.product_id;
 
